c40a5bc2823b8aee6ce3fe770b2c711fc37aef9a
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2025-01-24 14:53:41 -0500

    Update Dash_Code.py

--- Dash_Code.py
+++ Dash_Code.py
@@ -1930,7 +1930,6 @@ asset_list = asset_list[:15]
 asset_list = ['Total'] + asset_list
 
 asset_list_day = asset_fetch_day()
-asset_list_day = asset_list_day[:15]
 asset_list_day_2 = asset_list_day
 asset_list_day = ['Total'] + asset_list_day
 
