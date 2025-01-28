d29004e68ab5ca513607613a52d97d7523a84be0
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2025-01-28 14:15:40 -0500

    Update Dash_Code.py

--- Dash_Code.py
+++ Dash_Code.py
@@ -2113,7 +2113,7 @@ with col2:
 
 col1, col2 = st.columns(2)
 
-'''
+Place_Holder = '''
 selected_assets_hourly = st.multiselect("Select Assets", asset_list_day_2, default=asset_list_day_2[:4])
 
 col1, col2 = st.columns(2)
